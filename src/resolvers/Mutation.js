b53fa32bc27afa804f766cf63b1b22d13a888722
qlevaslot <quentin.levaslot@gmail.com>
2020-04-06 17:46:42 +0200

    add subscriptions

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -20,6 +20,21 @@ const updateLink = (root, {id, url, description}, context) => context.prisma.upd
 });
 const deleteLink = (root, {id}, context) => context.prisma.deleteLink({id});
 
+const vote = async (parent, args, context, info) => {
+  const userId = getUserId(context);
+  const voteExists = await context.prisma.$exists.vote({
+    user: { id: userId },
+    link: { id: args.linkId },
+  });
+  if (voteExists) {
+    throw new Error(`Already voted for link: ${args.linkId}`)
+  }
+  return context.prisma.createVote({
+    user: { connect: { id: userId } },
+    link: { connect: { id: args.linkId } },
+  });
+};
+
 const signup = async (parent, args, context, info) => {
   const password = await bcrypt.hash(args.password, 10);
   const user = await context.prisma.createUser({...args, password});
@@ -52,4 +67,5 @@ module.exports = {
   post,
   updateLink,
   deleteLink,
+  vote,
 };
