f964ff56360a81412776123d0d3c3c1352ebdfad
qlevaslot <quentin.levaslot@gmail.com>
2020-04-16 15:33:12 +0200

    add subscriptions to new links

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -6,8 +6,11 @@ import {ApolloClient} from 'apollo-client';
 import {createHttpLink} from 'apollo-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
+import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom';
 
 import * as serviceWorker from './serviceWorker';
 import './styles/index.css';
@@ -28,9 +31,28 @@ const authLink = setContext((_, { headers }) => {
   }
 });
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+  link,
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
